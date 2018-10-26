bdb45618caafc87dd0aa300a10d67532986219b4
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2018-10-26 09:58:52 -0400

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,14 @@
+public class JackOLantern {
+    private String [][] face;
+
+    public JackOLantern(String [][]face)
+    {
+        this.face = face;
+    }
+
+    public 
+
+
+
+
+}
---
+++ src/JackOLanternRunner.java
@@ -0,0 +1,7 @@
+public class JackOLanternRunner {
+    public static void main(String[] args)
+    {
+
+    }
+
+}
